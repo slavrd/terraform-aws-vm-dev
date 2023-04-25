e2b358def09fbb27d42d3f99e455099189821038
Slav Donchev <slav.donchev@gmail.com>
2023-04-25 10:32:17 +0300

    create multiple vms

--- main.tf
+++ main.tf
@@ -23,7 +23,17 @@ resource "aws_security_group_rule" "allow_ingress" {
   security_group_id = aws_security_group.aws_vm_dev.id
 }
 
+resource "aws_security_group_rule" "allow_ingress_sg" {
+  type              = "ingress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  self              = true
+  security_group_id = aws_security_group.aws_vm_dev.id
+}
+
 resource "aws_instance" "aws_vm_dev" {
+  count                       = var.vm_count
   ami                         = var.vm_image_id
   instance_type               = var.vm_type
   key_name                    = var.vm_key_name
@@ -33,6 +43,6 @@ resource "aws_instance" "aws_vm_dev" {
   user_data_base64            = var.vm_user_data_base64
 
   tags = {
-    "Name" = "${var.name_prefix}aws-vm-dev"
+    "Name" = "${var.name_prefix}aws-vm-dev-${count.index}"
   }
 }
